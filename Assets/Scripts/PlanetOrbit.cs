6b8d6cc9a91f387cfdc0d1a606c09aa4cf4746ae
tharukanandasiri <tharukanandasiri123@gmail.com>
2025-10-29 23:16:06 +0530

    Self-rotating planets

--- Assets/Scripts/PlanetOrbit.cs
+++ Assets/Scripts/PlanetOrbit.cs
@@ -6,11 +6,15 @@ public class PlanetOrbit : MonoBehaviour
 
     public float orbitSpeed;
 
+    public float rotationSpeed = 50.0f;
+
     void Update()
     {
         if (sun != null)
         {
             transform.RotateAround(sun.position, Vector3.up, orbitSpeed * Time.deltaTime);
+
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
         }
     }
 }
